2c34665a09cfe5158ef6030223623e3243f21f4a
Gene Liverman <me@technicalissues.us>
2015-12-16 01:40:51 -0500

    Made app "modular," updated for passenger.

--- tree_planter.rb
+++ tree_planter.rb
@@ -1,168 +1,173 @@
 require 'json'
+require 'logger'
 require 'open3'
-require 'pp'
-require 'sinatra'
+require 'sinatra/base'
 
-config_obj = JSON.parse(File.read((Dir.pwd) + "/config.json"))
+class TreePlanter < Sinatra::Base
 
-get '/' do
-  "<h1>Tree Planter</h1>
-   <h2>To use this tool you need to send a post to one of the following:</h2>
-   <ul>
-     <li>#{request.url}deploy</li>
-     <li>#{request.url}gitlab</li>
-     <li>#{request.url}hook-test</li>
-   </ul>"
-end
+  set :logging, true
 
-# Deploys a repository using the default branch
-post '/deploy' do
-  payload = JSON.parse(request.body.read)
-  logger.info("json payload: #{payload.inspect}")
+  config_obj = JSON.parse(File.read((Dir.pwd) + "/config.json"))
 
-  endpoint    = 'deploy'
-
-  if payload.has_key?("tree_name")
-    tree_name   = payload['tree_name']
-    branch_name = ''
-    repo_url    = payload['repo_url']
-
-    logger.info("endpoint  = #{endpoint}")
-    logger.info("tree name = #{tree_name}")
-    logger.info("repo url  = #{repo_url}")
-    logger.info("")
-  else
-    tree_name    = (payload['repository']['url'].split('/')[-1]).split('.')[0]
-    branch_name  = (payload['ref'].split('/')).drop(2).join('_')
-    repo_url     = payload['repository']['url']
-
-    logger.info("endpoint     = #{endpoint}")
-    logger.info("tree name    = #{tree_name}")
-    logger.info("repo url     = #{repo_url}")
-    logger.info("branch name  = #{branch_name}")
-    logger.info("")
+  get '/' do
+    "<h1>Tree Planter</h1>
+     <h2>To use this tool you need to send a post to one of the following:</h2>
+     <ul>
+       <li>#{request.url}deploy</li>
+       <li>#{request.url}gitlab</li>
+       <li>#{request.url}hook-test</li>
+     </ul>"
   end
 
+  # Deploys a repository using the default branch
+  post '/deploy' do
+    payload = JSON.parse(request.body.read)
+    logger.info("json payload: #{payload.inspect}")
+
+    endpoint    = 'deploy'
+
+    if payload.has_key?("tree_name")
+      tree_name   = payload['tree_name']
+      branch_name = ''
+      repo_url    = payload['repo_url']
+
+      logger.info("endpoint  = #{endpoint}")
+      logger.info("tree name = #{tree_name}")
+      logger.info("repo url  = #{repo_url}")
+      logger.info("")
+    else
+      tree_name    = (payload['repository']['url'].split('/')[-1]).split('.')[0]
+      branch_name  = (payload['ref'].split('/')).drop(2).join('_')
+      repo_url     = payload['repository']['url']
+
+      logger.info("endpoint     = #{endpoint}")
+      logger.info("tree name    = #{tree_name}")
+      logger.info("repo url     = #{repo_url}")
+      logger.info("branch name  = #{branch_name}")
+      logger.info("")
+    end
 
 
-  deploy_tree(endpoint, tree_name, branch_name, repo_url, config_obj)
-end
-
-# Parses the payload from GitLab and deploys a specific branch of a repository.
-post '/gitlab' do
-  payload = JSON.parse(request.body.read)
-  logger.info("json payload: #{payload.inspect}")
-
-  # Determine event type
-  if payload['ref'].split('/')[1].eql? "heads"
-    endpoint     = 'gitlab'
-    tree_name    = (payload['repository']['url'].split('/')[-1]).split('.')[0]
-    branch_name  = (payload['ref'].split('/')).drop(2).join('_')
-    repo_name    = payload['repository']['name']
-    repo_url     = payload['repository']['url']
-    checkout_sha = payload['checkout_sha']
-
-    logger.info("repo name    = #{repo_name}")
-    logger.info("repo url     = #{repo_url}")
-    logger.info("branch name  = #{branch_name}")
-    logger.info("checkout sha = #{checkout_sha}")
-    logger.info("")
 
     deploy_tree(endpoint, tree_name, branch_name, repo_url, config_obj)
-
-  elsif payload['ref'].split('/')[1].eql? "tags"
-    tag_name = payload['ref'].split('/')[2]
-    logger.info("tag = #{tag_name}")
   end
 
-end
+  # Parses the payload from GitLab and deploys a specific branch of a repository.
+  post '/gitlab' do
+    payload = JSON.parse(request.body.read)
+    logger.info("json payload: #{payload.inspect}")
 
-# Simply prints the
-post '/hook-test' do
-  payload = JSON.parse(request.body.read)
+    # Determine event type
+    if payload['ref'].split('/')[1].eql? "heads"
+      endpoint     = 'gitlab'
+      tree_name    = (payload['repository']['url'].split('/')[-1]).split('.')[0]
+      branch_name  = (payload['ref'].split('/')).drop(2).join('_')
+      repo_name    = payload['repository']['name']
+      repo_url     = payload['repository']['url']
+      checkout_sha = payload['checkout_sha']
 
-  logger.info('JSON Payload:')
-  logger.info("json payload: #{payload.inspect}")
-  logger.info('')
-  logger.info('request.env:')
-  logger.info(JSON.pretty_generate(request.env))
-end
+      logger.info("repo name    = #{repo_name}")
+      logger.info("repo url     = #{repo_url}")
+      logger.info("branch name  = #{branch_name}")
+      logger.info("checkout sha = #{checkout_sha}")
+      logger.info("")
 
-def deploy_tree(endpoint, tree_name, branch_name, repo_url, config_obj)
-  base         = config_obj['base_dir']
+      deploy_tree(endpoint, tree_name, branch_name, repo_url, config_obj)
 
-  stream do |body_content|
-    body_content << "endpoint: #{endpoint}\n"
-    body_content << "tree:     #{tree_name}\n"
-    body_content << "branch:   #{branch_name}\n"
-    body_content << "repo_url: #{repo_url}\n"
-    body_content << "base:     #{base}\n"
-    body_content << "\n"
+    elsif payload['ref'].split('/')[1].eql? "tags"
+      tag_name = payload['ref'].split('/')[2]
+      logger.info("tag = #{tag_name}")
+    end
 
-    logger.info("endpoint:    #{endpoint}")
-    logger.info("tree:        #{tree_name}")
-    logger.info("branch:      #{branch_name}")
-    logger.info("repo_url:    #{repo_url}")
-    logger.info("base:        #{base}")
+  end
 
-    if Dir.exists?(base)
-      Dir.chdir(base)
+  # Simply prints the
+  post '/hook-test' do
+    payload = JSON.parse(request.body.read)
 
-      case endpoint
-        when 'deploy'
-          repo_path = "#{tree_name}"
+    logger.info('JSON Payload:')
+    logger.info("json payload: #{payload.inspect}")
+    logger.info('')
+    logger.info('request.env:')
+    logger.info(JSON.pretty_generate(request.env))
+  end
 
-        when 'gitlab'
-          repo_path =("#{tree_name}_#{branch_name}")
+  def deploy_tree(endpoint, tree_name, branch_name, repo_url, config_obj)
+    base         = config_obj['base_dir']
 
+    stream do |body_content|
+      body_content << "endpoint: #{endpoint}\n"
+      body_content << "tree:     #{tree_name}\n"
+      body_content << "branch:   #{branch_name}\n"
+      body_content << "repo_url: #{repo_url}\n"
+      body_content << "base:     #{base}\n"
+      body_content << "\n"
+
+      logger.info("endpoint:    #{endpoint}")
+      logger.info("tree:        #{tree_name}")
+      logger.info("branch:      #{branch_name}")
+      logger.info("repo_url:    #{repo_url}")
+      logger.info("base:        #{base}")
+
+      if Dir.exists?(base)
+        Dir.chdir(base)
+
+        case endpoint
+          when 'deploy'
+            repo_path = "#{tree_name}"
+
+          when 'gitlab'
+            repo_path =("#{tree_name}_#{branch_name}")
+
+          else
+            repo_path = nil
+            abort("Failing... endpoint #{endpoint} unknown")
+        end
+        logger.info("repo_path:   #{repo_path}")
+
+        if !repo_path.nil?
+          repo_exists = Dir.exists?("./#{repo_path}")
         else
-          repo_path = nil
-          abort("Failing... endpoint #{endpoint} unknown")
-      end
-      logger.info("repo_path:   #{repo_path}")
+          repo_exists = nil
+          abort("No repo path was set.")
+        end
 
-      if !repo_path.nil?
-        repo_exists = Dir.exists?("./#{repo_path}")
+        if repo_exists
+          Dir.chdir(repo_path)
+          deploy_command = "git pull"
+        else
+          if branch_name.eql? ''
+            deploy_command = "git clone #{repo_url} #{repo_path}"
+          else
+            deploy_command = "git clone -b #{branch_name} --single-branch #{repo_url} #{repo_path}"
+          end
+        end
+
+        body_content << "Running #{deploy_command}\n"
+        logger.info("Running #{deploy_command}")
+
+        Open3.popen2e(deploy_command) do |stdin, stdout_err, wait_thr|
+
+          while line = stdout_err.gets
+            body_content << line
+            logger.info(line)
+          end
+
+          exit_status = wait_thr.value
+          if exit_status.success?
+            status 200
+            #body body_content
+          else
+            status 500
+            #body body_content
+          end
+        end # end Open3
       else
-        repo_exists = nil
-        abort("No repo path was set.")
+        status 500
+        msg = "#{base} cannot be found"
+        body_content << "#{msg}\n"
+        logger.info(msg)
       end
-
-      if repo_exists
-        Dir.chdir(repo_path)
-        deploy_command = "git pull"
-      else
-        if branch_name.eql? ''
-          deploy_command = "git clone #{repo_url} #{repo_path}"
-        else
-          deploy_command = "git clone -b #{branch_name} --single-branch #{repo_url} #{repo_path}"
-        end
-      end
-
-      body_content << "Running #{deploy_command}\n"
-      logger.info("Running #{deploy_command}")
-
-      Open3.popen2e(deploy_command) do |stdin, stdout_err, wait_thr|
-
-        while line = stdout_err.gets
-          body_content << line
-          logger.info(line)
-        end
-
-        exit_status = wait_thr.value
-        if exit_status.success?
-          status 200
-          #body body_content
-        else
-          status 500
-          #body body_content
-        end
-      end # end Open3
-    else
-      status 500
-      msg = "#{base} cannot be found"
-      body_content << "#{msg}\n"
-      logger.info(msg)
     end
   end
 end
